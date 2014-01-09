ec87abfca29b8de7e903da263b73ad7927950216
sijchen <sijchen@cisco.com>
2014-01-09 10:10:25 +0800

    Complete the SetRates function

--- media/webrtc/signaling/src/media-conduit/WebrtcOpenH264VideoCodec.cpp
+++ media/webrtc/signaling/src/media-conduit/WebrtcOpenH264VideoCodec.cpp
@@ -266,6 +266,30 @@ int32_t WebrtcOpenH264VideoEncoder::SetChannelParameters(uint32_t packetLoss,
 
 int32_t WebrtcOpenH264VideoEncoder::SetRates(uint32_t newBitRate,
                                              uint32_t frameRate) {
+  //update bitrate if needed
+  int32_t existEncoderBitRate = 0;
+  int rv = encoder_->GetOption(ENCODER_OPTION_BITRATE, &existEncoderBitRate);
+  int32_t newEncoderBitRate = newBitRate*1000; //kbps->bps
+  if ( rv==cmResultSuccess && existEncoderBitRate!=newEncoderBitRate ) {
+    rv = encoder_->SetOption(ENCODER_OPTION_BITRATE, &newEncoderBitRate);
+    //MOZ_MTLOG(ML_INFO, "Update Encoder Bandwidth: ReturnValue: " << rv << " BitRate(kbps): " << newBitRate);
+  }
+  if (rv!=cmResultSuccess)
+    return WEBRTC_VIDEO_CODEC_ERROR;
+  
+  //update framerate if needed
+  float existFrameRate = 0.0;
+  rv = encoder_->GetOption(ENCODER_OPTION_FRAME_RATE, &existFrameRate);
+  const float kfEpsn = 0.000001f;
+  if ( rv==cmResultSuccess &&
+      ( (frameRate-existFrameRate) > kfEpsn || (frameRate-existFrameRate) < -kfEpsn) ) {
+    float newFrameRate = static_cast<float>(frameRate);
+    rv = encoder_->SetOption(ENCODER_OPTION_FRAME_RATE, &newFrameRate);
+    //MOZ_MTLOG(ML_INFO, "Update Encoder Frame Rate: ReturnValue: " << rv << " FrameRate: " << frameRate);
+  }
+  if (rv!=cmResultSuccess)
+    return WEBRTC_VIDEO_CODEC_ERROR;
+    
   return WEBRTC_VIDEO_CODEC_OK;
 }
 
